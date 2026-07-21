97501cd4563fd6c82d2dfd2b60fd52fc538fdf34
HigorPassarela <hhpassarela@gmail.com>
2026-07-21 13:24:00 -0300

    bugfix: ajuste em configurações de security

--- account-service/src/main/java/com/bankhub/account/infrastructure/cache/AccountRedisSyncListener.java
+++ account-service/src/main/java/com/bankhub/account/infrastructure/cache/AccountRedisSyncListener.java
@@ -4,10 +4,9 @@ import com.bankhub.account.domain.event.AccountCreatedEvent;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
@@ -16,12 +15,12 @@ public class AccountRedisSyncListener {
 
     private final StringRedisTemplate redisTemplate;
 
-    private static final String REDIS_KEY_PREFIX = "status:account";
+    private static final String REDIS_KEY_PREFIX = "status:account:";
 
     /**
      * Ouve o evento de criação da conta após o sucesso no banco de dados.
      */
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @EventListener
     @Retry(name = "redisRetry", fallbackMethod = "fallbackRedisSync")
     public void syncAccountStatusToRedis(AccountCreatedEvent event) {
         String accountId = event.account().id();
